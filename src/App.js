ed1e0ebaef12f8f951b3140d2e8dfac098d5e375
Robert Kin <93863169+RobertKin@users.noreply.github.com>
2022-10-31 10:08:30 +0100

    Stilizajica navigacije dodata

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import React from "react";
+import Navbar from "./Sections/Header/Header";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
