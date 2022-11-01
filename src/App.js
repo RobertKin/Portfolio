c421f46d682546764ba20c5f7e1acb0ef7852c64
Robert Kin <93863169+RobertKin@users.noreply.github.com>
2022-11-01 14:39:53 +0100

    Novo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "./Sections/Header/Components/Nav";
-import Hero from "./Sections/Header/Components/Hero";
 import Head from "./Sections/Header/Header";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
       <Head />
     </div>
   );
--- src/Sections/Header/Header.jsx
+++ src/Sections/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import "../Header/Header.css"
+import "../Header/Header.css";
 
-function Head () {
-    return(
-        <div>Hellooo</div>
-    )
+function Head() {
+  return (
+    <div className="headImg">
+      <main className="main">
+        <div className="main_hero">
+          <h1>We are Front-end web developers</h1>
+          <h4>We make wonderful things</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+            perferendis inventore facilis, pariatur ad laboriosam sint omnis?
+            Sapiente, repellat commodi. Perspiciatis quibusdam animi temporibus
+            sunt repudiandae! Similique Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Neque dolore itaque et ipsum repellat maxime id
+            pariatur aliquid magni asperiores? corporis aliquam ut.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Head;
