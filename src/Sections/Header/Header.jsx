e5a084bc61f037aaa463553a193eaf1162627a0e
OmerAslani <aslaniomer21@gmail.com>
2022-11-02 12:42:21 +0100

    robet's job fixed

--- src/Sections/Header/Header.jsx
+++ src/Sections/Header/Header.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import "../Header/Header.css";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Nav";
 
 function Head() {
   return (
-    <div className="headImg">
-      <main className="main">
-        <div className="main_hero">
-          <h1>We are Front-end web developers</h1>
-          <h4>We make wonderful things</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            perferendis inventore facilis, pariatur ad laboriosam sint omnis?
-            Sapiente, repellat commodi. Perspiciatis quibusdam animi temporibus
-            sunt repudiandae! Similique Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Neque dolore itaque et ipsum repellat maxime id
-            pariatur aliquid magni asperiores? corporis aliquam ut.
-          </p>
-        </div>
-      </main>
+
+    <div className="header">
+      <div id="robert">
+      <Navbar />
+      <Hero />
+      </div>
+      
+      
     </div>
+
+    
+  
   );
 }
 
