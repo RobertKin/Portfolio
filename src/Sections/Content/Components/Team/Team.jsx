cead64b8734206f34d074bf0eb844a4d20533f5c
Robert Kin <93863169+RobertKin@users.noreply.github.com>
2022-11-02 15:02:33 +0100

    Dodat: Team and Work page

--- src/Sections/Content/Components/Team/Team.jsx
+++ src/Sections/Content/Components/Team/Team.jsx
@@ -3,7 +3,22 @@ import "../Team/Team.css"
 
 function Team () {
     return(
-        <div className="team"></div>
+        <div className="team">
+            <div className="zero">
+            <div className="zero-b">
+              <h1>We are Front-end web developers</h1>
+              <h4>We make wonderful things</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+                perferendis inventore facilis, pariatur ad laboriosam sint omnis?
+                Sapiente, repellat commodi. Perspiciatis quibusdam animi temporibus
+                sunt repudiandae! Similique Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Neque dolore itaque et ipsum repellat maxime id
+                pariatur aliquid magni asperiores? corporis aliquam ut.
+              </p>
+            </div>
+          </div>
+        </div>
     )
 }
 
