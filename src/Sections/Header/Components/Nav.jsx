562e1680d412d5ff8f1f315ef014453f386898ba
Robert Kin <93863169+RobertKin@users.noreply.github.com>
2022-11-05 16:55:35 +0100

    Dodela

--- src/Sections/Header/Components/Nav.jsx
+++ src/Sections/Header/Components/Nav.jsx
@@ -22,7 +22,7 @@ function Navbar() {
       </a>
       <ul className={active}>
         <li className="nav_item">
-          <a href="#box" className="nav_link">
+          <a href="#" className="nav_link">
             Home
           </a>
         </li>
@@ -32,7 +32,7 @@ function Navbar() {
           </a>
         </li>
         <li className="nav_item">
-          <a href="#" className="nav_link">
+          <a href="#box3" className="nav_link">
             Portfolio
           </a>
         </li>
