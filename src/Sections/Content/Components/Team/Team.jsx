26ead4ed330de4a2f5c0f679e87c22e364ac50e5
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-11-06 15:11:19 +0100

    data.json

--- src/Sections/Content/Components/Team/Team.jsx
+++ src/Sections/Content/Components/Team/Team.jsx
@@ -1,9 +1,10 @@
-import React from "react";
 import "../Team/Team.css";
 
+import React from "react";
+import data from "../../../../data.json";
 
 function Team() {
-
+  const { omer, robert } = data.aboutus;
 
   return (
     <div className="team">
@@ -11,12 +12,10 @@ function Team() {
         <div className="box-a">
           <h1>Creative and Professional</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            perferendis inventore facilis, pariatur ad laboriosam sint omnis?
-            Sapiente, repellat commodi. Perspiciatis quibusdam animi temporibus
-            sunt repudiandae! Similique Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Neque dolore itaque et ipsum repellat maxime id
-            pariatur aliquid magni asperiores? corporis aliquam ut.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perferendis inventore facilis, pariatur ad laboriosam
+            sint omnis? Sapiente, repellat commodi. Perspiciatis quibusdam animi temporibus sunt repudiandae! Similique Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Neque dolore itaque et ipsum repellat maxime id pariatur aliquid
+            magni asperiores? corporis aliquam ut.
           </p>
         </div>
 
@@ -25,31 +24,21 @@ function Team() {
           <div className="pic-container">
             <div className="img-1"></div>
             <div className="info-container">
-              <h4>Omer Aslani</h4>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-                eaque.
-              </p>
+              <h4>{omer.name}</h4>
+              <p>{omer.description}</p>
             </div>
-
           </div>
-
 
           <div className="pic-container">
             <div className="img-2"></div>
             <div className="info-container">
               <h4>Robert Firanji</h4>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-                eaque.
-              </p>
+              <p>{robert.description}</p>
             </div>
           </div>
-
         </div>
       </div>
     </div>
-
   );
 }
 
